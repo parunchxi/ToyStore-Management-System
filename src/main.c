2cdee07f907431b16f1d9d2d35c64c01f475a06c
parunchxi <parunchxi@gmail.com>
2023-12-02 20:38:57 +0700

    Update UI

--- src/main.c
+++ src/main.c
@@ -16,7 +16,8 @@ void delay(int milliseconds)
 int main()
 {
     int user = 0;
-    int choice[10] = {-1};
+    int choice[5] = {-1, -1, -1, -1, -1};
+
     do
     {
         banner();
@@ -34,7 +35,57 @@ int main()
                 {
                 case 1:
                     // Admin
-                    break;
+                    do
+                    {
+                        banner();
+                        header("Admin Menu");
+                        printf("1. View Store Report\n");
+                        printf("2. Display All Toys\n");
+                        printf("3. Add a New Toy\n");
+                        printf("4. Remove a Toy\n");
+                        printf("5. Update Toy Details\n");
+                        printf("6. Search for Toys\n");
+                        printf("7. Back Up Database\n");
+                        printf("8. Restore Database\n");
+                        printf("0. Logout\n");
+                        printf(">> ");
+                        scanf("%d", &choice[1]);
+                        switch (choice[1])
+                        {
+                        case 1:
+                            // View Store Report
+                            break;
+                        case 2:
+                            // Display All Toys
+                            break;
+                        case 3:
+                            // Add a New Toy
+                            break;
+                        case 4:
+                            // Remove a Toy
+                            break;
+                        case 5:
+                            // Update Toy Details
+                            break;
+                        case 6:
+                            // Search for Toys
+                            break;
+                        case 7:
+                            // Back Up Database
+                            break;
+                        case 8:
+                            // Restore Database
+                            break;
+                        case 0:
+                            danger("Logging out...");
+                            delay(1000);
+                            break;
+                        default:
+                            danger("Invalid choice!");
+                            printf("Press enter to continue...");
+                            getch();
+                        }
+                    } while (choice[1] != 0);
                 case 2:
                     // Cashier
                     break;
@@ -42,12 +93,14 @@ int main()
                     printf("1. Try again\n");
                     printf("0. Exit\n");
                     printf(">> ");
-                    scanf("%d", &choice[1]);
-                    switch (choice[1])
+                    scanf("%d", &choice[0]);
+                    switch (choice[0])
                     {
                     case 1:
                         continue;
                     case 0:
+                        danger("Exiting...");
+                        delay(1000);
                         return 0;
                     default:
                         danger("Invalid choice!");
@@ -55,15 +108,17 @@ int main()
                         getch();
                     }
                 }
-            } while (choice[1] != 0);
+            } while (choice[0] != 0);
         case 0:
+            danger("Exiting...");
+            delay(1000);
             return 0;
         default:
             danger("Invalid choice!");
             printf("Press enter to continue...");
             getch();
         }
-    } while (choice[0] != -1);
+    } while (choice[0] != 0);
 
     return 0;
 }
